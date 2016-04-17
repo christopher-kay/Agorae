64d531d43a8d264597134c5e6b02224ff2508949
christopher-kay <christopher.kay1@utt.fr>
2016-04-17 22:52:22 +0200

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,62 +1,45 @@
 require 'capybara/rspec'
-require 'capybara/webkit'
+require 'capybara/poltergeist'
 
 Capybara.run_server = false
-Capybara.default_driver = :webkit
-Capybara.default_wait_time = 5
-Capybara.app_host = 
-  'http://127.0.0.1:5984/tire-a-part/_design/tire-a-part/_rewrite'
+Capybara.default_driver = :poltergeist
+Capybara.app_host = "http://agorae.test.hypertopic.org"
 
-RSpec.configure do |config|
-  config.before(:each) do
-    prefer_language 'fr'
+$home_page = '/'
+
+def in_dialog()
+  find('#dialog')
+end
+
+def log_in_as(login, password)
+  fill_in "Nom d'utilisateur", :with => login
+  fill_in 'Mot de passe', :with => password
+  in_dialog.click_on 'Se connecter'
+  expect(page).not_to have_content 'Connectez-vous'
+end
+
+def toggle_edit()
+  find('#toggle').click
+end
+
+def click_plus_sign_next_to(list)
+  find(".#{list}-list .add").click
+end
+
+def click_last(list)
+  find("##{list} li:last-child .editable").click
+end
+
+def click_on_link(text)
+  find('span', :text => "#{text}").click
+end
+
+def type(*keys_sequences)
+  keys_sequences.each do |s|
+    find('input[type=textbox]').native.send_keys s
   end
 end
 
 def a_string()
   s = ('a'..'z').to_a.shuffle[0,8].join
-end
-
-def in_dialog()
-  f = find('.ui-dialog')
-end
-
-def prefer_language(language)
-  page.driver.header 'Accept-Language', language
-end
-
-def sample(name)
-  IO.read("spec/samples/#{name}.bib")
-end
-
-def field(id)
-  find_by_id(id).value
-end 
-
-# for select boxes handled by Chosen
-def fill_in_and_select(value, options)
-  options[:visible] = false
-  select value, options
-end
-
-def wait_until
-  require "timeout"
-  Timeout.timeout(Capybara.default_wait_time) do
-    sleep(0.1) until value = yield
-    value
-  end
-end
-
-def downloaded_file_name
-  wait_until {page.response_headers['Content-Disposition']}.partition('=').last
-end
-
-def check_path_content(content)
-  uri = URI.parse(current_url)
-  uri.path.should == content
-end
-
-def check_query_content(content)
-  uri = URI.parse(current_url)
-  url.query.should == content
 end
